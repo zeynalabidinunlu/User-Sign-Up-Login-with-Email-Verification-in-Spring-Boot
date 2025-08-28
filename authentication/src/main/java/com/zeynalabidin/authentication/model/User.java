1bcb601c9c0712bcc322c4768fd27554fb4d5b5c
zeynalabidinunlu <zeynalunlu@gmail.com>
2025-08-28 21:56:58 +0300

    AuthService and EmailService completed

--- authentication/src/main/java/com/zeynalabidin/authentication/model/User.java
+++ authentication/src/main/java/com/zeynalabidin/authentication/model/User.java
@@ -1,85 +1,70 @@
 package com.zeynalabidin.authentication.model;
 
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
-public class User implements UserDetails{
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	
-	
-	@Column(unique = true, nullable = false)
-	private String username;
-	
-	@Column(unique = true,nullable = false)
-	private String email;
-	
-	@Column(nullable = false)
-	private String password;
-	
-	private boolean enabled;
-	
-	@Column(name = "verification_code")
-	private String verificationCode;
-	
-	@Column(name = "verification_expiration")
-	private LocalDateTime verificationCodeExpiredAt;
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
 
-	public User(String username, String email, String password) {
-		super();
-		this.username = username;
-		this.email = email;
-		this.password = password;
-	}
-	
-	
-	public User() {
-		
-	}
+    @Column(name = "verification_code")
+    private String verificationCode;
+    @Column(name = "verification_expiration")
+    private LocalDateTime verificationCodeExpiresAt;
+    private boolean enabled;
 
+    //constructor for creating an unverified user
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    //default constructor
+    public User(){
+    }
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return List.of();
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
-	
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-		
-	}
-	
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
-	
-	@Override
-	public boolean isEnabled() {
-		return enabled ;
-	}
+    //TODO: add proper boolean checks
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
